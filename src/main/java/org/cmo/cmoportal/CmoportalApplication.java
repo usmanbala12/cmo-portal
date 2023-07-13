2050b08031ce076ad30d14fe9f9eb434a2ad8084
codegeek <usmanxp12@gmail.com>
2023-07-13 17:57:47 +0100

    second commit

---
+++ src/main/java/org/cmo/cmoportal/CmoportalApplication.java
@@ -0,0 +1,13 @@
+package org.cmo.cmoportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmoportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmoportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cmo/cmoportal/CmoportalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cmo.cmoportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmoportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
